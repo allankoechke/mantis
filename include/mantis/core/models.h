02507153e7814b59f6b4046c33c45ffd2bf996d4
Allan K. Koech <allankoech.ke@gmail.com>
2025-05-09 00:54:10 +0300

    Project restructure: Moved model declaration to .cpp file

--- include/mantis/core/models.h
+++ include/mantis/core/models.h
@@ -8,45 +8,126 @@
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
-// Enum of the table type created,
-// base table types provide `index`, `created`, `updated`
-// auth table type provide `base` type + `email`, `password`, `name`
-// view table type provide readonly `sql`
-typedef enum TableType
+namespace Mantis
 {
-    BaseTable = 1,
-    AuthTable,
-    ViewTable
-} TableType;
+    // Enum of the table type created,
+    // base table types provide `index`, `created`, `updated`
+    // auth table type provide `base` type + `email`, `password`, `name`
+    // view table type provide readonly `sql`
+    typedef enum TableType
+    {
+        BaseTable = 1,
+        AuthTable,
+        ViewTable
+    } TableType;
 
-struct BaseTable
-{
-    std::string id;
-    uint64_t created_at;
-    uint64_t updated_at;
+    NLOHMANN_JSON_SERIALIZE_ENUM(TableType, {
+        {TableType::BaseTable, "base"},
+        {TableType::AuthTable, "auth"},
+        {TableType::ViewTable, "view"}
+    })
 
-    json listRule = "";
-    json getRule = "";
-    json addRule = "";
-    json updateRule = "";
-    json deleteRule = "";
-};
+    // Text, Number, Bool, etc.
+    // Enum to represent different data types for fields
+    enum class FieldType {
+        Url,
+        Text,
+        Integer,
+        Float,
+        Boolean,
+        DateTime,
+        Email,
+        Password,
+        JSON
+    };
 
-struct AuthTable: BaseTable
-{
-    std::string email;
-    std::string name;
-    std::string password;
-};
+    NLOHMANN_JSON_SERIALIZE_ENUM(FieldType, {
+        { FieldType::Url,       "url" },
+        { FieldType::Text,      "text" },
+        { FieldType::Integer,   "integer" },
+        { FieldType::Float,     "float" },
+        { FieldType::Boolean,   "boolean" },
+        { FieldType::DateTime,  "datetime" },
+        { FieldType::Email,     "email" },
+        { FieldType::Password,  "password" },
+        { FieldType::JSON,      "json" }
+    })
 
-struct ViewTable
-{
-    std::string sql;
+    // Access Rule Struct
+    typedef struct Rule
+    {
+        std::string expression;
+        bool enabled;
 
-    json listRule = "";
-    json getRule = "";
-};
+        json to_json() const;
+    } Rule;
 
+    // Field definition
+    struct Field {
+        std::string name;
+        FieldType type;
 
+        bool required = false;
+        bool primaryKey = false;
+        bool system = false;
+
+        std::optional<std::string> defaultValue;     // as string, parse based on type
+        std::optional<int> minValue;
+        std::optional<int> maxValue;
+        std::optional<std::string> autoGeneratePattern; // regex for auto-gen strings
+
+        Field() = default;
+
+        // Convenience constructor
+        Field(std::string n, const FieldType t, const bool req = false, const bool pk = false, const bool sys = false);
+
+        [[nodiscard]] json to_json() const;
+        [[nodiscard]] std::string to_sql() const;
+    };
+
+    // Represents a generic table in the system
+    struct Table {
+        std::string id;
+        std::string name;
+        TableType type;
+        std::string schema;
+
+        std::vector<Field> fields;
+
+        Rule listRule;
+        Rule getRule;
+        Rule addRule;
+        Rule updateRule;
+        Rule deleteRule;
+
+        virtual json to_json() const;
+
+        virtual std::string to_sql() const;
+    };
+
+    // Specific model for Base table (user-defined)
+    struct BaseTable : Table {
+        bool enableSync = true;
+
+        BaseTable();
+    };
+
+    // Specific model for Auth table
+    struct AuthTable : Table {
+        std::string usernameField = "email";
+        std::string passwordField = "password";
+        bool enableSync = true;
+
+        AuthTable();
+    };
+
+    // Specific model for View table
+    struct ViewTable : Table {
+        std::string sourceSQL;
+        bool enableSync = false;
+
+        ViewTable();
+    };
+}
 
 #endif //MODELS_H
---
+++ src/models.cpp
@@ -0,0 +1,117 @@
+//
+// Created by allan on 09/05/2025.
+//
+
+#include <mantis/mantis.h>
+#include <mantis/core/models.h>
+
+json Mantis::Rule::to_json() const
+{
+    return json{{"expression", expression}, {"enabled", enabled}};
+}
+
+Mantis::Field::Field(std::string n, const FieldType t, const bool req, const bool pk, const bool sys)
+    : name(std::move(n)), type(t), required(req), primaryKey(pk), system(sys)
+{}
+
+json Mantis::Field::to_json() const
+{
+    return {
+        { "name", name },
+        { "type", type },
+        { "required", required },
+        { "primaryKey", primaryKey },
+        { "system", system },
+        { "defaultValue", defaultValue },
+        { "minValue", minValue },
+        { "maxValue", maxValue },
+        { "autoGeneratePattern", autoGeneratePattern }
+    };
+}
+
+std::string Mantis::Field::to_sql() const
+{
+    std::string sql_type;
+    switch (type) {
+    case FieldType::Url:        sql_type = "TEXT";      break;
+    case FieldType::Text:       sql_type = "TEXT";      break;
+    case FieldType::Email:      sql_type = "TEXT";      break;
+    case FieldType::Integer:     sql_type = "INTEGER";   break;
+    case FieldType::Float:     sql_type = "REAL";   break;
+    case FieldType::Boolean:    sql_type = "BOOLEAN";   break;
+    case FieldType::DateTime:   sql_type = "DATETIME";  break;
+    case FieldType::Password:   sql_type = "TEXT";      break;
+    case FieldType::JSON:       sql_type = "JSON";      break;
+    }
+
+    std::string sql = name + " " + sql_type;
+
+    if (primaryKey) sql += " PRIMARY KEY";
+    if (required) sql += " NOT NULL";
+    if (defaultValue.has_value()) sql += " DEFAULT '" + *defaultValue + "'";
+
+    return sql;
+}
+
+json Mantis::Table::to_json() const
+{
+    json j;
+    j["id"] = id;
+    j["name"] = name;
+    j["type"] = type;
+    j["schema"] = schema;
+    j["fields"] = json::array();
+
+    for (const auto& f : fields) j["fields"].push_back(f.to_json());
+
+    j["rules"] = {
+        {"list", listRule.to_json()},
+        {"get", getRule.to_json()},
+        {"add", addRule.to_json()},
+        {"update", updateRule.to_json()},
+        {"delete", deleteRule.to_json()}
+    };
+
+    return j;
+}
+
+std::string Mantis::Table::to_sql() const
+{
+    std::string sql = "CREATE TABLE IF NOT EXISTS " + name + " (";
+
+    for (size_t i = 0; i < fields.size(); ++i) {
+        sql += fields[i].to_sql();
+        if (i < fields.size() - 1) sql += ", ";
+    }
+    sql += ");";
+
+    return sql;
+}
+
+Mantis::BaseTable::BaseTable()
+{
+    type = TableType::BaseTable;
+    fields = {
+        Field("id", FieldType::Text, true, true, true),
+        Field("created", FieldType::DateTime, true, false, true),
+        Field("updated", FieldType::DateTime, true, false, true)
+    };
+}
+
+Mantis::AuthTable::AuthTable()
+{
+    type = TableType::AuthTable;
+    fields = {
+        Field("id", FieldType::Text, true, true, true),
+        Field("created", FieldType::DateTime, true, false, true),
+        Field("updated", FieldType::DateTime, true, false, true),
+        Field("email", FieldType::Email, true, false, true),
+        Field("password", FieldType::Password, true, false, true),
+        Field("name", FieldType::Text)
+    };
+}
+
+Mantis::ViewTable::ViewTable()
+{
+    type = TableType::ViewTable;
+}
