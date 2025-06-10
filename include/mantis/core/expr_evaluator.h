5f495490cad8e542b46cf13d0047a72375dbdfea
Allan K. Koech <allankoech.ke@gmail.com>
2025-06-10 22:36:34 +0300

    Refactor: cparse refactor to avoid enum clashing; Auth middleware reworking

---
+++ include/mantis/core/expr_evaluator.h
@@ -0,0 +1,16 @@
+//
+// Created by allan on 09/06/2025.
+//
+
+#ifndef EXPR_EVALUATOR_H
+#define EXPR_EVALUATOR_H
+
+namespace mantis {
+
+class expr_evaluator {
+
+};
+
+} // mantis
+
+#endif //EXPR_EVALUATOR_H
