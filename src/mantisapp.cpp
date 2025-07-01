39504c3db29f85df3e28a1027f251908f88b825b
Allan K. Koech <allankoech.ke@gmail.com>
2025-07-01 23:03:42 +0300

    Bug fix for release build on GitHub
    Minor refactoring and renaming
    
    Bumped to v0.1.4-alpha

---
+++ src/mantisapp.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file mantisapp.cpp
+ * @brief Standalone `mantisapp` executable entrypoint
+ *
+ * Created by allan on 08/05/2025.
+ */
+
+#include "../include/mantis/app/app.h"
+
+/**
+ * @brief MantisApp standalone entrypoint
+ * @param argc Argument count
+ * @param argv Argument list
+ * @return Non-zero if the application did not exit correctly
+ */
+int main(const int argc, char* argv[])
+{
+    // Create `MantisApp` instance with the passed in arguments
+    mantis::MantisApp app(argc, argv);
+
+    // Initialize the app, this ensures all `Mantis` units are
+    // initialized and running.
+    app.init();
+
+    // Run the http server listening loop
+    return app.run();
+}
