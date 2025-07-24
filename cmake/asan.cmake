e643e612204f012a8c40a177d57b50ee68a14f23
Allan K. Koech <allankoech.ke@gmail.com>
2025-07-25 00:53:30 +0300

    Update asan.cmake

--- cmake/asan.cmake
+++ cmake/asan.cmake
@@ -2,7 +2,7 @@
 option(ENABLE_ASAN "Enable AddressSanitizer" OFF)
 
 function(enable_asan_for_target target)
-    if(ENABLE_ASAN)
+    if(ENABLE_ASAN AND NOT WIN32)
         message(STATUS "Enabling ASan for target ${target}")
         target_compile_options(${target} PRIVATE -fsanitize=address -fno-omit-frame-pointer)
         target_link_options(${target} PRIVATE -fsanitize=address)
