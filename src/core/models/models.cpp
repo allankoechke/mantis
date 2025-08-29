8cffcced9ca78bf64a977896c80fb940d446be6d
Allan K. Koech <allankoech.ke@gmail.com>
2025-08-30 02:55:59 +0300

    Core: PosgreSQL |> Fixed db_type not supported for soci's db_uint8 and db_int8 types

--- src/core/models/models.cpp
+++ src/core/models/models.cpp
@@ -69,23 +69,23 @@ mantis::json mantis::Validator::validate(const std::string& key, const std::stri
 
 std::optional<mantis::FieldType> mantis::getFieldType(const std::string& fieldName)
 {
-    if (fieldName == "xml")     return FieldType::XML;
-    if (fieldName == "string")  return FieldType::STRING;
-    if (fieldName == "double")  return FieldType::DOUBLE;
-    if (fieldName == "date")    return FieldType::DATE;
-    if (fieldName == "int8")    return FieldType::INT8;
-    if (fieldName == "uint8")   return FieldType::UINT8;
-    if (fieldName == "int16")   return FieldType::INT16;
-    if (fieldName == "uint16")  return FieldType::UINT16;
-    if (fieldName == "int32")   return FieldType::INT32;
-    if (fieldName == "uint32")  return FieldType::UINT32;
-    if (fieldName == "int64")   return FieldType::INT64;
-    if (fieldName == "uint64")  return FieldType::UINT64;
-    if (fieldName == "blob")    return FieldType::BLOB;
-    if (fieldName == "json")    return FieldType::JSON;
-    if (fieldName == "bool")    return FieldType::BOOL;
-    if (fieldName == "file")    return FieldType::FILE;
-    if (fieldName == "files")    return FieldType::FILES;
+    if (fieldName == "xml") return FieldType::XML;
+    if (fieldName == "string") return FieldType::STRING;
+    if (fieldName == "double") return FieldType::DOUBLE;
+    if (fieldName == "date") return FieldType::DATE;
+    if (fieldName == "int8") return FieldType::INT8;
+    if (fieldName == "uint8") return FieldType::UINT8;
+    if (fieldName == "int16") return FieldType::INT16;
+    if (fieldName == "uint16") return FieldType::UINT16;
+    if (fieldName == "int32") return FieldType::INT32;
+    if (fieldName == "uint32") return FieldType::UINT32;
+    if (fieldName == "int64") return FieldType::INT64;
+    if (fieldName == "uint64") return FieldType::UINT64;
+    if (fieldName == "blob") return FieldType::BLOB;
+    if (fieldName == "json") return FieldType::JSON;
+    if (fieldName == "bool") return FieldType::BOOL;
+    if (fieldName == "file") return FieldType::FILE;
+    if (fieldName == "files") return FieldType::FILES;
     return std::nullopt;
 }
 
@@ -115,9 +115,11 @@ bool mantis::fieldExists(const TableType& type, const std::string& fieldName)
 bool mantis::isValidFieldType(const std::string& fieldType)
 {
     // Valid field types for our backend
-    std::vector<std::string> fieldTypes{"xml", "string", "double", "date",
-        "int8", "uint8","int16", "uint16", "int32", "uint32", "int64", "uint64",
-        "blob", "json", "bool", "file", "files"};
+    std::vector<std::string> fieldTypes{
+        "xml", "string", "double", "date",
+        "int8", "uint8", "int16", "uint16", "int32", "uint32", "int64", "uint64",
+        "blob", "json", "bool", "file", "files"
+    };
 
     const auto it = std::ranges::find(fieldTypes, fieldType);
     return it != fieldTypes.end();
@@ -221,7 +223,11 @@ soci::db_type mantis::Field::toSociType(const FieldType& f_type)
     if (f_type == FieldType::UINT64) return soci::db_uint64;
     if (f_type == FieldType::BLOB) return soci::db_blob;
     if (f_type == FieldType::BOOL) return soci::db_int8;
-    return soci::db_string;
+    if (f_type == FieldType::STRING || f_type == FieldType::JSON
+        || f_type == FieldType::FILE || f_type == FieldType::FILES)
+        return soci::db_string;
+
+    throw std::invalid_argument("Unknown field type");
 }
 
 json mantis::Table::to_json() const
@@ -247,22 +253,44 @@ json mantis::Table::to_json() const
 
 std::string mantis::Table::to_sql() const
 {
+    // Lambda function to handle any special case data type handling, this is for
+    // generation of the SQL statement only not db data conversion.
+    auto get_field_type = [](const FieldType& t, const std::shared_ptr<soci::session>& sql) -> std::string
+    {
+        const auto db_type = sql->get_backend()->get_backend_name();
+        // For date types, enforce `text` type SQLite ONLY
+        if (db_type == "sqlite3" && t == FieldType::DATE)
+        {
+            return "text";
+        }
+
+        // PostgreSQL has no support for `db_uint` and `db_uint8` so lets store them in `db_uint16`/`db_int16 types`
+        if (db_type == "postgresql" && (t == FieldType::UINT8 || t == FieldType::INT8))
+        {
+            return t == FieldType::UINT8
+                       ? sql->get_backend()->create_column_type(soci::db_uint16, 0, 0)
+                       : sql->get_backend()->create_column_type(soci::db_int16, 0, 0);
+        }
+
+        // General catch for all other types
+        return sql->get_backend()->create_column_type(Field::toSociType(t), 0, 0);
+    };
+
+
     // Get DB Session
     const auto sql = MantisApp::instance().db().session();
 
     std::ostringstream ddl;
     ddl << "CREATE TABLE IF NOT EXISTS " << name << " (";
 
+
     for (size_t i = 0; i < fields.size(); ++i)
     {
         if (i > 0) ddl << ", ";
         const auto field = fields[i];
+
         ddl << field.name << " "
-            << (field.type == FieldType::DATE
-                    ? (sql->get_backend()->get_backend_name() == "sqlite3"
-                           ? "text"
-                           : sql->get_backend()->create_column_type(field.toSociType(), 0, 0))
-                    : sql->get_backend()->create_column_type(field.toSociType(), 0, 0));
+            << get_field_type(field.type, sql);
 
         if (field.primaryKey) ddl << " PRIMARY KEY";
         if (field.required) ddl << " NOT NULL";
