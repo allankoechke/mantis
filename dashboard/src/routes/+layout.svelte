dedc6f7808339a87af1f839f14c118286226a38f
Allan K. Koech <allankoech.ke@gmail.com>
2025-10-15 18:32:45 +0300

    Dashboard: Incremental migration to svelte dashboard

---
+++ dashboard/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/mantis.png';
+  import { ModeWatcher } from "mode-watcher";
+	
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher />
+{@render children?.()}
