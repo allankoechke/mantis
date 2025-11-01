1039bf0fc58f2dd0a532e11374a6e2a9e3c47dc7
Allan K. Koech <allankoech.ke@gmail.com>
2025-11-01 18:00:04 +0300

    Core: Revert httpblib config changes to fix ::GetWinAddrInfo missing function

--- cmake/add-httplib.cmake
+++ cmake/add-httplib.cmake
@@ -1,6 +1,8 @@
 # Build static lib for httplib
 set(HTTPLIB_COMPILE OFF CACHE BOOL "")
-if(NOT WIN32)
+if(WIN32)
+    set(HTTPLIB_USE_NON_BLOCKING_GETADDRINFO OFF)
+else (WIN32)
     set(HTTPLIB_REQUIRE_ZSTD TRUE)
     include(cmake/FindZSTD.cmake)
     find_package(zstd REQUIRED)
@@ -8,8 +10,6 @@ if(NOT WIN32)
     if(zstd_FOUND)
         add_library(zstd::libzstd ALIAS ZSTD::ZSTD)
     endif()
-
-    set(HTTPLIB_USE_NON_BLOCKING_GETADDRINFO OFF)
 endif()
 
 # Disable SSL use for now, throws errors with WolfSSL library version
